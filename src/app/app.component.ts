65ef87ec6ffdce8f3586cef05fb95e97a7d82698
XSronHou <houxsron@gmail.com>
2020-11-03 19:03:24 -0600

    Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'upload-file';
+  private file: File = null;
+  constructor(private http: HttpClient) {}
+  handleFileInput(files: FileList) {
+    this.file = files.item(0)
+  }
+  uploadFile() {
+    const formData: FormData = new FormData();
+    formData.append('file', this.file, this.file.name);
+    this.http.post("https://upload-service-ess.herokuapp.com/api/uploadImage", formData).subscribe((result) => {
+      alert(JSON.stringify(result))
+    }, error => {
+      alert(JSON.stringify(error))
+    })
+  }
 }
